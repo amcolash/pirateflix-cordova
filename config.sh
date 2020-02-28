d9a268cc214bf5d6cf0fa297dbd1b6f89e0960e2
Andrew McOlash <amcolash@gmail.com>
2020-02-27 23:58:08 -0800

    move dev/prod files around and make a simple copy script

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'ERROR usage: config.sh [dev/prod]'
+    exit 1
+fi
+
+cp $1/config.xml .
+cp $1/banner.png platforms/android/app/src/main/res/drawable-land-xhdpi/banner.png
