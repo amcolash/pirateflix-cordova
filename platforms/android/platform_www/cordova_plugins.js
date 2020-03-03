4faf89080a8e00cc1c315926b403e7f6150db60c
Andrew McOlash <amcolash@gmail.com>
2020-03-02 22:45:47 -0800

    add app info plugin

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -31,12 +31,21 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "CacheClear"
       ]
+    },
+    {
+      "id": "cordova-plugin-appinfo.AppInfo",
+      "file": "plugins/cordova-plugin-appinfo/www/appinfo.js",
+      "pluginId": "cordova-plugin-appinfo",
+      "merges": [
+        "navigator.appInfo"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-dialogs": "2.0.2",
     "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-splashscreen": "5.0.3",
-    "cordova-plugin-cache-clear": "1.3.7"
+    "cordova-plugin-cache-clear": "1.3.7",
+    "cordova-plugin-appinfo": "2.1.2"
   };
 });
---
+++ platforms/android/platform_www/plugins/cordova-plugin-appinfo/www/appinfo.js
@@ -0,0 +1,60 @@
+cordova.define("cordova-plugin-appinfo.AppInfo", function(require, exports, module) {
+
+var exec = require('cordova/exec');
+var channel = require('cordova/channel');
+
+channel.createSticky('onAppInfoReady');
+channel.waitForInitialization('onAppInfoReady');
+
+function appInfo() {
+
+    this.version = null;
+    this.identifier = null;
+    this.build = null;
+
+    var me = this;
+
+    channel.onCordovaReady.subscribe(function() {
+        me.getAppInfo(function(info) {
+            me.version = info.version;
+            me.identifier = info.identifier;
+            me.build = info.build || 'unknown';
+            channel.onAppInfoReady.fire();
+        },function(e) {           
+            console.log("[ERROR] Error initializing Cordova: " + e);
+        });
+    });
+}
+
+/**
+ * Get an object with the keys 'version', 'build' and 'identifier'.
+ *
+ * @param {Function} success    Callback method called on success.
+ * @param {Function} fail       Callback method called on failure.
+ */
+appInfo.prototype.getAppInfo = function(success, fail){
+    exec(success, fail, 'AppInfo', 'getAppInfo', []);
+};
+
+/**
+ * Get the version name.
+ *
+ * @param {Function} success    Callback method called on success.
+ * @param {Function} fail       Callback method called on failure.
+ */
+appInfo.prototype.getVersion = function(success, fail) {
+    exec(success, fail, 'AppInfo', 'getVersion', []);
+}
+
+/**
+ * Get the app identifier.
+ *
+ * @param {Function} success    Callback method called on success.
+ * @param {Function} fail       Callback method called on failure.
+ */
+appInfo.prototype.getIdentifier = function(success, fail){
+    exec(success, fail, 'AppInfo', 'getIdentifier', []);
+}
+
+module.exports = new appInfo();
+});
