27db5f45b2247ffd52eddfcc4d18c064b5b12ee6
Andrew McOlash <amcolash@gmail.com>
2020-03-02 21:41:19 -0800

    nicer config script, reorder plugins

--- config.sh
+++ config.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]] ; then
-    echo 'ERROR usage: config.sh [dev/prod]'
+    echo 'ERROR usage: config.sh [dev/prod] [[run]]'
     exit 1
 fi
 
 cp $1/config.xml ./config.xml
 cp $1/banner.png platforms/android/app/src/main/res/drawable-land-xhdpi/banner.png
 cp $1/index.html www/index.html
+
+if [[ $# -eq 2 ]] ; then
+    cordova run
+fi
