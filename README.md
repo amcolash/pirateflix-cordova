6736530c01f182a3a3a05e82e4c57c3e5e49d6af
Andrew McOlash <amcolash@gmail.com>
2021-06-20 16:41:58 -0700

    massively clean up manual configs, add config to package.json

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # pirateflix-cordova
+
 Android TV companion app for transmission-yify/pirate-flix
+
+## Building
+
+### Install Deps
+
+- Install node dependencies with `$ npm install`
+
+- Install android sdk and have `ANDROID_HOME` env var set
+
+### Set Up Cordova
+
+- Install cordova `$ npm i -g cordova`
+- Add android platform folder `$ cordova platforms add android`
+
+### Choose Dev/Prod
+
+The dev build points to a local server while the prod one points to prod server. Update the `production` value inside of `package.json`.
+When this value changes, you will need to rebuild the `platforms` folder. Easiest route is `$ npm run clean`.
+
+### Build / Deploy with Cordova
+
+```bash
+$ npm run build
+
+# OR
+
+$ npm run deploy
+```
---
+++ hooks/before_prepare/copy_resources.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { join } = require('path');
+const package = require('../../package.json');
+
+module.exports = function (context) {
+  // Configure current build type
+  const BuildType = {
+    DEV: 'dev',
+    PROD: 'prod',
+  };
+  const BUILD = package.production ? BuildType.PROD : BuildType.DEV;
+
+  console.log(`Build type: ${BUILD}`);
+
+  // Native resources to copy
+  const srcResPath = 'res/android';
+
+  // Android platform resource path
+  const androidResPath = 'platforms/android/app/src/main/res/drawable-land-xhdpi';
+
+  // List of files to copy
+  const toCopy = [{ src: `${srcResPath}/banner-${BUILD}.png`, dest: `${androidResPath}/banner.png` }];
+
+  toCopy.forEach((f) => {
+    fs.copyFileSync(f.src, f.dest);
+    console.log(`Copied ${f.src} --> ${f.dest}`);
+  });
+
+  const indexPath = 'www/index.html';
+  const index = fs
+    .readFileSync(indexPath)
+    .toString()
+    .replace(/\(.*Url\)/, `(${BUILD}Url)`);
+  fs.writeFileSync(indexPath, index);
+
+  const configPath = 'config.xml';
+  const config = fs
+    .readFileSync(configPath)
+    .toString()
+    .replace(/<widget id=".*?"/, `<widget id="com.amcolash.pirateflix${BUILD === BuildType.DEV ? '_dev' : ''}"`)
+    .replace(/<name>.*?<\/name>/, `<name>PirateFlix${BUILD === BuildType.DEV ? ' - Dev' : ''}</name>`);
+  fs.writeFileSync(configPath, config);
+};
